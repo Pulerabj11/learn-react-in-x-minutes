365422a9cd69dd6c3eb81c5b5afaf03ee070736e
Brandon Pulera <pulerabj11@gmail.com>
2021-07-27 13:07:56 -0500

    Setting up the todo list and importing it to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import TodoList from "./TodoList";
+
 function App() {
-  return null
+  return (
+    <>
+      <TodoList />
+      <input type="text"/>
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
